9ff48b3e8e041cf75ae9f653bb820eb4bb911c03
rd1127 <57807589+rd1127@users.noreply.github.com>
2019-11-18 16:11:45 +0000

    movement
    made movement for img

--- kroy/core/src/com/dicycat/kroy/Kroy.java
+++ kroy/core/src/com/dicycat/kroy/Kroy.java
@@ -1,7 +1,10 @@
 package com.dicycat.kroy;
 
+import java.awt.RenderingHints.Key;
+
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -9,19 +12,37 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class Kroy extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	int x;
+	int y;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		x = 0;
+		y = 0;
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		if (Gdx.input.isKeyPressed(Keys.UP)) {
+			y = y + 4;
+		}
+		if (Gdx.input.isKeyPressed(Keys.DOWN)) {
+			y = y - 4;
+		}
+		if (Gdx.input.isKeyPressed(Keys.RIGHT)) {
+			x = x + 4;
+		}
+		if (Gdx.input.isKeyPressed(Keys.LEFT)) {
+			x = x - 4;
+		}
+		
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, x, y);
 		batch.end();
 	}
 	
