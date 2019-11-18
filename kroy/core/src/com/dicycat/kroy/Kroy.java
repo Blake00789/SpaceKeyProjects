2d6c9c9ef1b5b73bc5ef441603a7515ba5fb7174
corro-s <56735728+corro-s@users.noreply.github.com>
2019-11-18 16:25:35 +0000

    Revert "Merge branch 'master' of https://github.com/Mic714/DicyCat"
    This reverts commit 047092fb1c7ac3b87ede471119188130e63b5725, reversing
    changes made to 6909dc54000e45536da7e384af62f6a069b76d6a.

--- kroy/core/src/com/dicycat/kroy/Kroy.java
+++ kroy/core/src/com/dicycat/kroy/Kroy.java
@@ -1,10 +1,7 @@
 package com.dicycat.kroy;
 
-import java.awt.RenderingHints.Key;
-
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -12,37 +9,19 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class Kroy extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	int x;
-	int y;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
-		x = 0;
-		y = 0;
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 1, 1, 1);
+		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		
-		if (Gdx.input.isKeyPressed(Keys.UP)) {
-			y = y + 4;
-		}
-		if (Gdx.input.isKeyPressed(Keys.DOWN)) {
-			y = y - 4;
-		}
-		if (Gdx.input.isKeyPressed(Keys.RIGHT)) {
-			x = x + 4;
-		}
-		if (Gdx.input.isKeyPressed(Keys.LEFT)) {
-			x = x - 4;
-		}
-		
 		batch.begin();
-		batch.draw(img, x, y);
+		batch.draw(img, 0, 0);
 		batch.end();
 	}
 	
