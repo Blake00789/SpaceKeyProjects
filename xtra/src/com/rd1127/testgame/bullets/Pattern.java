84b9696fe4da3eee0bd92ff14952954e88eea0de
rd1127 <57807589+rd1127@users.noreply.github.com>
2019-11-19 18:28:01 +0000

    Xtra
    Added game screen, game objects, entities and bullets

---
+++ xtra/src/com/rd1127/testgame/bullets/Pattern.java
@@ -0,0 +1,13 @@
+package com.rd1127.testgame.bullets;
+
+public class Pattern {
+	Bullet[] bullets;
+	float timeBetweenBullets;
+	
+	public Pattern(Bullet[] _bullets, float _time) {
+		bullets = _bullets;
+		timeBetweenBullets = _time;
+	}
+	
+	
+}
