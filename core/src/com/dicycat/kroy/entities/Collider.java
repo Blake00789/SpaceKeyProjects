de38d2b107c87843d2fce58d7e7ba88fa4744cf3
corro-s <56735728+corro-s@users.noreply.github.com>
2019-11-21 14:39:38 +0000

    Test for Dan

--- core/src/com/dicycat/kroy/entities/Collider.java
+++ core/src/com/dicycat/kroy/entities/Collider.java
@@ -1,16 +1,25 @@
 package com.dicycat.kroy.entities;
 
 import com.badlogic.gdx.math.Circle;
+import com.badlogic.gdx.math.Rectangle;
 
 public class Collider {
 
+    //TODO: Not going to be void, need to work out representation.
+    public static void getRectangleHitbox(Rectangle x, float angle){
+
+    }
+
     public static boolean CircleOnCircle(Circle c1, Circle c2){
         if(c1.overlaps(c2)){
-            return True;
+            return true;
         }
         else{
-            return False;
+            return false;
         }
     }
 
+    public static boolean RectangleOnRectangle(){
+
+    }
 }
