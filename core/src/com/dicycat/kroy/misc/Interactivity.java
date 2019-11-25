d67df8d4cfe430ee60c82bd040725ab2300591a9
IsaacAlbiston <ia737@york.ac.uk>
2019-11-25 20:14:53 +0000

    Create Interactivity.java
    Added Interactivity interface

---
+++ core/src/com/dicycat/kroy/misc/Interactivity.java
@@ -0,0 +1,21 @@
+package com.dicycat.kroy.misc;
+
+import com.badlogic.gdx.math.Vector2;
+import com.dicycat.kroy.GameObject;
+import com.dicycat.kroy.entities.FireTruck;
+import com.dicycat.kroy.screens.GameScreen;
+
+public interface Interactivity {
+	
+	public default boolean PlayerInRadius(int x, int y, int range, GameScreen gscreen) {
+		Vector2 tempVector = new Vector2();
+		FireTruck player = gscreen.getPlayer();
+		tempVector = player.GetCentre();
+		tempVector = tempVector.sub(x,y);
+		if (range>= tempVector.len()) {
+			return(true);
+		}
+		return(false);
+	}
+	
+}
