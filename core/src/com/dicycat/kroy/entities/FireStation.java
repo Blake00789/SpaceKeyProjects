98aea17a2c01d99600ce5085e1b3efb2df839830
corro-s <56735728+corro-s@users.noreply.github.com>
2019-12-20 20:43:18 +0000

    Added refill function to station, see description for further work.
    - Health function needs adding.
    - Station needs positioning and the original sprite removed from game map.

--- core/src/com/dicycat/kroy/entities/FireStation.java
+++ core/src/com/dicycat/kroy/entities/FireStation.java
@@ -1,7 +1,9 @@
 package com.dicycat.kroy.entities;
 
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
+import com.dicycat.kroy.screens.GameScreen;
 
 public class FireStation extends Entity {
 
@@ -10,4 +12,18 @@ public class FireStation extends Entity {
 			
 	}
 
+	public void Update(){
+		if(playerInRadius()){
+			GameScreen.mainGameScreen.GetPlayer().increaseWater();
+		}
+	}
+
+	public Boolean playerInRadius() {
+		Vector2 currentCoords = GameScreen.mainGameScreen.GetPlayer().GetCentre(); // get current player coordinates
+		if (Vector2.dst(currentCoords.x, currentCoords.y, GetCentre().x, GetCentre().y) < radius ) { // checks the distance between the two entities
+			return true; // returns true if distance between entity and player is less than radius of item
+		}else {
+			return false; // returns false otherwise
+		}
+	}
 }
