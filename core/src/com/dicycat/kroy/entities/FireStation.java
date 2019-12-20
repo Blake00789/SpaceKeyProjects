5deb9eae7dc28c1126607ac4c22015433b0cec5b
Luke T <lnt511@york.ac.uk>
2019-12-20 17:30:40 +0000

    Healthpoint Fix
    Added to FireStation and Fortress Class

--- core/src/com/dicycat/kroy/entities/FireStation.java
+++ core/src/com/dicycat/kroy/entities/FireStation.java
@@ -6,7 +6,7 @@ import com.badlogic.gdx.math.Vector2;
 public class FireStation extends Entity {
 
 	public FireStation(Vector2 spawnPos) {
-		super(spawnPos, new Texture("FireStationTemp.png"), new Vector2(298,175));
+		super(spawnPos, new Texture("FireStationTemp.png"), new Vector2(298,175), Integer.MAX_VALUE);
 			
 	}
 
