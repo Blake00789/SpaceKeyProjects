f62f2344654da0fa39d81a7f8444927c93c4e580
sch556 <sch556@york.ac.uk>
2020-02-14 13:40:54 +0000

    Added comments for minigame code, implemented game over screen

--- core/src/com/dicycat/kroy/entities/Goose.java
+++ core/src/com/dicycat/kroy/entities/Goose.java
@@ -9,21 +9,30 @@ import com.badlogic.gdx.math.Vector2;
 import com.dicycat.kroy.Kroy;
 
 /**
- * Goose class for minigame
+ * Goose class for minigame, acts like the bird in flappy bird
  * 
  * @author Sam Hutchings
  *
  */
 public class Goose extends Entity {
-	private float deceleration = 0.5f;
-	private float velocity = 0;
-	private Rectangle hitbox = new Rectangle(0, 0, 48 * scale, 56 * scale);
-	private static float scale = 1.5f;
+	private float deceleration = 0.5f; // Gravity on the goose
+	private float velocity = 0; // Speed of the goose falling or flapping
+	private Rectangle hitbox = new Rectangle(0, 0, 46 * scale, 54 * scale); // Hitbox of the goose
+	private static float scale = 1.5f; // Allows the goose to be scaled up in size
 
+	/**
+	 * @param spawnPos Where the goose spawns
+	 * @param img The texture of the goose
+	 * @param imSize The size of the goose
+	 * @param health The health of the goose unnecessary
+	 */
 	public Goose(Vector2 spawnPos, Texture img, Vector2 imSize, int health) {
 		super(spawnPos, img, imSize, health);
 	}
 
+	/**
+	 * Instantiate the default goose
+	 */
 	public Goose() {
 		this(new Vector2(-32 * scale, -32 * scale), new Texture("goose2.png"), new Vector2(64 * scale, 64 * scale), 1);
 	}
@@ -48,6 +57,9 @@ public class Goose extends Entity {
 		}
 	}
 
+	/**
+	 * Draw debug rectangle of the hitbox 
+	 */
 	public void debugDraw() {
 		Kroy.mainMinigameScreen.DrawRect(new Vector2(hitbox.x, hitbox.y), new Vector2(hitbox.width, hitbox.height), 2,
 				Color.GREEN);
@@ -67,6 +79,9 @@ public class Goose extends Entity {
 		return scale;
 	}
 
+	/**
+	 * @return the hitbox
+	 */
 	public Rectangle getHitbox() {
 		return this.hitbox;
 	}
