4daca52dde5367440082e29ebf4c326082064ee5
jtt508 <56657233+jtt508@users.noreply.github.com>
2020-02-14 23:10:36 +0000

    changed back to the old tilemap

--- core/src/com/dicycat/kroy/gamemap/TiledGameMap.java
+++ core/src/com/dicycat/kroy/gamemap/TiledGameMap.java
@@ -24,6 +24,7 @@ public class TiledGameMap{
 	 * 
 	 */
 	public TiledGameMap() {
+		
 		tiledMap = new TmxMapLoader().load("YorkMap.tmx");
 		tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
 	}
