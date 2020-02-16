c2d884139ebfbb719b0df68b42a9edd73c5aa8c7
sch556 <sch556@york.ac.uk>
2020-02-16 23:38:36 +0000

    Update website

--- core/src/com/dicycat/kroy/screens/MenuScreen.java
+++ core/src/com/dicycat/kroy/screens/MenuScreen.java
@@ -45,7 +45,7 @@ public class MenuScreen implements Screen{
   private OptionsWindow optionsWindow;
   
   public static Music music = Gdx.audio.newMusic(Gdx.files.internal("gamemusic.mp3"));
-  public static float musicVolume = 0f;
+  public static float musicVolume = 0.4f;
 
   //coordinates for Play and Exit buttons 
   private int buttonWidth = 250;
