ccdeaaa9e2f7aeb302b7f100d538b0e9cd2169b2
Lucy Ivatt <li571@york.ac.uk>
2020-03-19 17:06:55 +0000

    fixed warping at edge of map and replace with black colour

--- core/src/com/dicycat/kroy/Kroy.java
+++ core/src/com/dicycat/kroy/Kroy.java
@@ -1,6 +1,8 @@
 package com.dicycat.kroy;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.dicycat.kroy.screens.GameScreen;
 import com.dicycat.kroy.screens.MenuScreen;
@@ -36,6 +38,8 @@ public class Kroy extends Game {
 
 	@Override
 	public void render () {
+		Gdx.gl.glClearColor( 0, 0, 0, 1 );
+		Gdx.gl.glClear( GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT );
 		super.render();
 	}
 	
