ed99d87444771a9e8d1fee16007a79e0a4e45bbc
GeckoGamez <alasdairpilmorebedford@gmail.com>
2020-03-31 00:34:27 +0100

    AllDamagedFortressesAdded

--- core/src/com/dicycat/kroy/GameTextures.java
+++ core/src/com/dicycat/kroy/GameTextures.java
@@ -13,40 +13,36 @@ import com.badlogic.gdx.graphics.Texture;
  *
  */
 public class GameTextures {
-	private Texture ufo, fireStation, fireStationDead, bullet;
+	private Texture truck, ufo, fireStation, fireStationDead, bullet;
+
 	private Texture[] livingFortresses = {new Texture("cliffords tower.png"),
 			new Texture("york minster.png"), new Texture("Memorial.png") ,
 			new Texture("york museum.png") , new Texture("University.png"),
 			new Texture("Station.png")};
 
-	private Texture[] deadFortresses = {new Texture("cliffords tower dead.png"),
-			new Texture("york minster dead.png"), new Texture("york museum dead.png"),
-			new Texture("york museum dead.png"), new Texture("york minster dead.png"),
-			new Texture("york museum dead.png")};
-
-	// REFACTOR_1 - START OF MODIFICATION  - NP STUDIOS - LUCY IVATT
-	// Changed the array of strings containing the file names to an array of textures contaning all textures required
-	// for the 6 trucks within the game to keep consistent as the above are arrays of textures and not filenames.
 	private Texture[] truckTextures = {new Texture ("fireTruck1.png"), new Texture("fireTruck2.png"),
 			new Texture("fireTruck3.png"), new Texture("fireTruck4.png"),
 			new Texture("fireTruck5.png"), new Texture("fireTruck6.png")};
-	// REFACTOR_1 - END OF MODIFICATION - NP STUDIOS
 
-	// REFACTOR_2 - START OF MODIFICATION  - NP STUDIOS - LUCY IVATT
-	// Removed truck texture here as all 6 are now required it was an unused variable left by the previous team.
-	// Also removed 'truckNum' parameter which was used to get the correct texture previously. Also deleted bullet
-	// texture as we removed the static access within the bullet class to aid testing.
-	public GameTextures() {
 	// REFACTOR_2 - END OF MODIFICATION - NP STUDIOS
+	private Texture[] deadFortresses = {new Texture("cliffords tower dead.png"),
+			new Texture("york minster dead.png"), new Texture("MemorialDamaged.png"),
+			new Texture("york museum dead.png"), new Texture("UniversityDamaged.png"),
+			new Texture("StationDamaged.png")};
+	private String[] truckAddress = {"fireTruck1.png", "fireTruck2.png", "fireTruck3.png", "fireTruck4.png", "fireTruck5.png", "fireTruck6.png"};
+	  
+	 
+	/**
+	 * @param truckNum Which truck texture to get
+	 */
+	public GameTextures() {
 		ufo = new Texture("ufo.png");
 		fireStation = new Texture("FireStationTemp.png");
 		fireStationDead = new Texture("FireStationTempDead.png");
 		bullet = new Texture(("bullet.png"));
 	}
 
-	// REFACTOR_3 - START OF MODIFICATION  - NP STUDIOS - LUCY IVATT
-	// Edited to work with the new texture array compared to the previous filename strings as well as improving the
-	// Javadoc associated with this method.
+
 	/**
 	 * Gets the trucks texture from the array truckTextures
 	 * @param truckNum the index of the truck's texture that is needed
